71058a43d1d49b4b29f8152701904c8f0b7c5446
branrile09 <93716194+branrile09@users.noreply.github.com>
2024-02-29 19:53:37 +1030

    First big commit
    this our first commit

--- PASS 2024 T1/Program.cs
+++ PASS 2024 T1/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("this is our first project!!! WOOO!!!!");
+
+
+
         }
 
 
