f51aa4bb631956dfae4e8e18b892094226504acf
branrile09 <93716194+branrile09@users.noreply.github.com>
2024-02-29 20:50:44 +1030

    PASS Workshop
    OUR FIRST WORKSHOP!!!!

--- PASS 2024 T1/Program.cs
+++ PASS 2024 T1/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace PASS_2024_T1
+﻿namespace PASS_2024_T
 {
     internal class Program
     {
@@ -7,9 +7,82 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("this is our first project!!! WOOO!!!!");
 
+            Console.WriteLine("hehehehehehe");
+            Console.WriteLine("yaygithub");
+            Console.WriteLine("yaygithub");
 
+            int jake = 31;
+            string jakey = "math101 is hard";
+            Console.WriteLine(jakey);
+            Console.Beep();
+
+            //Khan Academy is a good learning platform for math
+            //saved my HSC lol:)
+            // Thank you! Working through it slowly lol khan is great
+
+            Console.WriteLine("noob to slightly less noob (~RB)");
+            Console.WriteLine("YES");
+            Console.WriteLine("Hello PASS2024");
+
+
+            // numeric Variables
+
+            float hehe = 2.3f;
+
+            double x = 3;
+
+            long t = 686879;
+
+            int b = 1;
+
+            jake = b + 1;
+
+            //prob does not count as variable? static void?
+            double newNum = Math.Pow(x, 2);
+
+            
+
+            short e = 2;
+
+            //string and char variables
+
+            char money = '$';
+
+
+            string mod2 = "Welcome to Module 2 of Introduction to Software Engineering!" +
+                "\r\n\r\nIn Module 1, you had learnt how to set up the Visual Studio environment" +
+                " to create, store and execute C# programs/projects. In addition to that, you had also " +
+                "implemented your first C# program along with the computer programming concepts.\r\n\r\nThis " +
+                "module brings to you another set of very important programming fundamental concepts, such as data " +
+                "types, comments and input/output.";
+            Console.WriteLine(mod2);
+
+            double storedNum = outputNumbers(x, newNum);
+            Console.WriteLine(storedNum);
+            Console.WriteLine(newNum);
+            //bools
+
+            bool happy = true;
+
+
+
+
+
+
+
+
+            //self documenting code example
 
         }
+
+
+        static double outputNumbers(double i, double j)
+        {
+
+            return i + j;      
+        
+        }
+
 
 
 
